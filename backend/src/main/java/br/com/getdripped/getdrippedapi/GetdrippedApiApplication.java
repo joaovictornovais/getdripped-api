114c1ce44ae42c61eb8ed7a151e808a2d6efce5b
João Victor Novais <joao.dipaay@gmail.com>
2023-07-21 15:31:58 -0300

    Projeto inicializado

---
+++ backend/src/main/java/br/com/getdripped/getdrippedapi/GetdrippedApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.getdripped.getdrippedapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetdrippedApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetdrippedApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/getdripped/getdrippedapi/GetdrippedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.getdripped.getdrippedapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetdrippedApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
